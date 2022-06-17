ba59bbee2251a51eaa1238365309f1614dadde85
horamupa <horamupa@gmail.com>
2022-06-17 17:50:12 +0300

    Initial Commit

---
+++ guessTheFlagUI.edition/guessTheFlagUI_editionApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessTheFlagUI_editionApp.swift
+//  guessTheFlagUI.edition
+//
+//  Created by MM on 17.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct guessTheFlagUI_editionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
