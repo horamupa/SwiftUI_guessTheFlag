3694e95fa2817c25bb4da9c32243427903447105
horamupa <horamupa@gmail.com>
2022-06-20 14:38:32 +0300

    Good

--- guessTheFlagUI.edition/ContentView.swift
+++ guessTheFlagUI.edition/ContentView.swift
@@ -8,10 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var showingScore = false
+    @State var score = 0
+    @State var scoreTitle = ""
+    
+    @State var contries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    
+    @State var rightCountry = Int.random(in: 0...2)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            
+            Color.blue
+                .ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+                
+                VStack{
+                    Text("Choose the flag of ")
+                    Text("\(contries[rightCountry])")
+                }.foregroundColor(.white)
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        check(number)
+                    } label: {
+                        Image(contries[number])
+                                                .renderingMode(.original)
+                    }
+                }
+            }
+        }.alert(scoreTitle, isPresented: $showingScore) {
+            Button("Ask question", action: restart)
+            } message: {
+                Text("Your score is: \(score)")
+        
+            }
+            }
+    
+    func check(_ number: Int) {
+        if number == rightCountry {
+            scoreTitle = "Correct :)"
+            score += 1
+        } else {
+            scoreTitle = "Wrong :("
+        }
+        showingScore = true
     }
+    
+    func restart() {
+        contries.shuffle()
+        rightCountry = Int.random(in: 0...2)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
